f24c0916beac785feab226675707c251d6dddd52
Scott Johnson <scottmacaulayjohnson@googlemail.com>
2021-08-18 16:50:48 +0100

    yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,69 +1,53 @@
-class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+ class RestaurantsController < ApplicationController
+ before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
-  # GET /restaurants or /restaurants.json
-  def index
-    @restaurants = Restaurant.all
-  end
+   def index
+     @restaurants = Restaurant.all
+   end
 
-  # GET /restaurants/1 or /restaurants/1.json
-  def show
-  end
+   def show
+   end
 
-  # GET /restaurants/new
-  def new
-    @restaurant = Restaurant.new
-  end
+   def new
+     @restaurant = Restaurant.new
+   end
 
-  # GET /restaurants/1/edit
-  def edit
-  end
 
-  # POST /restaurants or /restaurants.json
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
+   def edit
+   end
 
-    respond_to do |format|
+   def create
+     @restaurant = Restaurant.new(restaurant_params)
       if @restaurant.save
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully created." }
-        format.json { render :show, status: :created, location: @restaurant }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+       redirect_to restaurants_path, notice: 'Restaurant was successfully created.'
+     else
+       render :new
+     end
+   end
 
-  # PATCH/PUT /restaurants/1 or /restaurants/1.json
-  def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully updated." }
-        format.json { render :show, status: :ok, location: @restaurant }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /restaurants/1 or /restaurants/1.json
-  def destroy
-    @restaurant.destroy
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+   def update
+    if @restaurant.update(restaurant_params)
+       redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+     else
+       render :edit
+     end
+   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :stars)
-    end
+   def destroy
+     @restaurant.destroy
+     redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+   end
+
+   private
+
+   def set_restaurant
+     @restaurant = Restaurant.find(params[:id])
+   end
+
+   def restaurant_params
+     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+   end
+
 end
--- db/migrate/20210818132435_create_restaurants.rb
+++ db/migrate/20210818132435_create_restaurants.rb
@@ -3,7 +3,8 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
     create_table :restaurants do |t|
       t.string :name
       t.string :address
-      t.integer :stars
+      t.string :phone_number
+      t.string :category
 
       t.timestamps
     end
